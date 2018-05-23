9e9d2515ccda41b3c827e05ce19071fada58fede
Robert Fox <inhumanthree1@protonmail.com>
2018-05-23 15:51:02 -0700

    Fixed init

--- coin/__init__.py
+++ coin/__init__.py
@@ -1,1 +1,16 @@
 name = "coin"
+import random
+def flip():
+    ans = random.randint(0,1)
+    if ans == 0:
+        return "Heads"
+    if ans == 1:
+        return "Tails"
+def bet():
+    ans = random.randint(0,1)
+    choice = input("Pick between 0 and 1: ")
+    if ans == choice:
+        return("Win")
+    else:
+        return("Loss")
+
